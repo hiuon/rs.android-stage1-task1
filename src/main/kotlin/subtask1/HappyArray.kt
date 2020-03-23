5bbab2cef95f546be84003313858a0c4a6310cda
Vector <hiuon.x@gmail.com>
2020-03-23 16:06:39 +0300

    feat: happyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,23 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
+        var happyArray = sadArray.toMutableList()
+        var i = 1
+        while (i < happyArray.size-1){
+            if (happyArray[i-1] + happyArray[i+1] < happyArray[i]){
+                happyArray.removeAt(i)
+                i--
+                if (i < 1){
+                    i++
+                }
+            }
+            else{
+                i++
+            }
+
+
+        }
+        return happyArray.toIntArray()
         throw NotImplementedError("Not implemented")
     }
 }
