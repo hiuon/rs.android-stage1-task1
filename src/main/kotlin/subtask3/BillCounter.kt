7110f9fc41f1c0356110338385fd163771007893
Vector <hiuon.x@gmail.com>
2020-03-23 16:34:08 +0300

    feat: billCounter

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -5,6 +5,12 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
+        var sumAnn = bill.sum()/2 - bill[k]/2
+        return if (sumAnn == b){
+            "Bon Appetit"
+        } else {
+            (b - sumAnn).toString()
+        }
         throw NotImplementedError("Not implemented")
     }
 }
