3f710e121d5093c8ba93bdde29a8230dff1cb9b1
Vector <hiuon.x@gmail.com>
2020-03-23 16:13:28 +0300

    feat: minMaxSum

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -4,6 +4,9 @@ class MiniMaxSum {
 
     // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
+        var max = input.max()
+        var min = input.min()
+        return arrayOf(input.sum() - max!!, input.sum() - min!!).toIntArray()
         throw NotImplementedError("Not implemented")
     }
 }
