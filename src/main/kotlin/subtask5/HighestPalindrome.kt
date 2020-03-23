e4daa2d7ad3681d19ff65a12b0cc0cd2f0690898
Vector <hiuon.x@gmail.com>
2020-03-23 17:11:57 +0300

    feat: highestPalindrome

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -4,6 +4,36 @@ class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
+        var digitString1 = StringBuilder(digitString)
+        var counter = 0
+        var highest = -1
+        var index = ArrayList<Int>()
+        for (i in 0 until digitString1.length/2){
+
+            if (digitString[i] != digitString1[digitString.length - 1 - i]){
+                index.add(i)
+                index.add(digitString.length - 1 - i)
+                if (highest < digitString1[i].toInt()){
+                    highest = digitString1[i].toInt()
+                    for (j in 0 until index.size){
+                        digitString1[index[j]] = highest.toChar()
+                    }
+                }
+                else if(highest < digitString1[digitString.length - 1 - i].toInt()){
+                    highest = digitString1[digitString.length - 1 - i].toInt()
+                    for (j in 0 until index.size){
+                        digitString1[index[j]] = highest.toChar()
+                    }
+                }
+                counter++
+            }
+        }
+        return if(counter > k){
+            "-1"
+        }
+        else{
+            digitString1.toString()
+        }
         throw NotImplementedError("Not implemented")
     }
 }
